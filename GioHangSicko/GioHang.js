3b622afc6d3a54a3c568d9a5c36beaf6eccc41f1
odt204 <thuanon11@gmail.com>
2023-10-11 12:26:10 +0700

    Add project files.

---
+++ GioHangSicko/GioHang.js
@@ -0,0 +1,23 @@
+﻿let amountElement = document.getElementById('amount');
+let amount = amountElement.value;
+console.log(amount);
+
+let render = (amount) => {
+    amountElement.value = amount
+}
+let handlePlus = () => {
+    amount++
+    render(amount);
+}
+let handleMinus = () => {
+    amount--
+    render(amount);
+}
+amountElement.addEventListener('input', () => {
+    amount = amountElement.value;
+    amount = parseInt(amount);
+    amount = (isNaN(amount) || amount == 0) ? 1 : amount;
+    render(amount);
+    console.log(amount);
+});
+
